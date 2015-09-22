be96e351f90d7b597db6d38cb0309078dd6e095a
Itexico <itexico@login.itexico.net>
2015-09-22 15:27:23 -0500

    hostname

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'http://ec2-52-88-74-47.us-west-2.compute.amazonaws.com',
+        hostname: 'ec2-52-88-74-47.us-west-2.compute.amazonaws.com',
         livereload: 35729
       },
       livereload: {
